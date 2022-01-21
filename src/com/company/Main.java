d79a236a23f143f50522bda772c80ca332e76f34
rishabhkesarwani <rkesari2002@gmail.com>
2022-01-21 16:15:54 +0530

    second Commit with new file

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,5 +6,6 @@ public class Main {
 	// write your code here
         System.out.println("This is basic code to test the features of git and github");
         System.out.println("This is basic code");
+        System.out.println("This is the first change ");
     }
 }
