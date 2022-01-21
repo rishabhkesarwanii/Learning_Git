8aaa25ef8cbd0350b63f4c4902bf9cc7638c03fa
rishabhkesarwani <rkesari2002@gmail.com>
2022-01-21 16:08:30 +0530

    First comit

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("This is basic code to test the features of git and github");
+        System.out.println("This is basic code");
+    }
+}
