bf67d3ae55f0ac9b53fd6d89b44a5ba6c0ed54e9
rishabhkesarwani <rkesari2002@gmail.com>
2022-01-21 16:27:58 +0530

    Mutiply feature

---
+++ src/com/company/mutilpy.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class mutilpy {
+
+    public static void main(String[] args){
+        int a = 12;
+        int b = 13;
+        System.out.println(a * b);
+    }
+}
