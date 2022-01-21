119a210f192f0e00a9d50249000e8a61f65674a4
rishabhkesarwani <rkesari2002@gmail.com>
2022-01-21 16:21:10 +0530

    new sum feature

--- src/com/company/sum.java
+++ src/com/company/sum.java
@@ -2,5 +2,12 @@ package com.company;
 
 public class sum {
 
+    public static void main(String[] args){
+        int a = 12;
+        int b = 13;
+        System.out.println(a + b);
+    }
+
+
 }
 
